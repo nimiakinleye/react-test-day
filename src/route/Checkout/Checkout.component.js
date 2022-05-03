95304320d9613471b57a817399df067c83bb1c0d
nimiakinleye <toluwanimiakinleye01@gmail.com>
2022-05-03 19:32:17 +0100

    can now dynamically get list of steps listed

--- src/route/Checkout/Checkout.component.js
+++ src/route/Checkout/Checkout.component.js
@@ -1,13 +1,62 @@
 import ContentWrapper from 'Component/ContentWrapper';
+import './Checkout.override.style.scss';
 
 import { Checkout as SourceCheckout } from 'SourceRoute/Checkout/Checkout.component'
 
 export class CheckoutComponent extends SourceCheckout {
 
-  renderTitle () {
-    return 'Hello'
+  renderSteps () {
+    const steps = this.stepMap
+    const stepsKey = Object.keys(steps)
+    console.log(stepsKey)
+    const stepsList = stepsKey.map(step => {
+      return (steps[step])
+    })
+    console.log(stepsList)
+
+    return (
+      stepsList.map(step => {
+        return (
+          <div block='Checkout' elem='Step'>{ step.title }</div>
+        )
+      })
+    )
+  }
+
+  render () {
+    return (
+      <main block="Checkout">
+                <ContentWrapper
+                wrapperMix={ { block: 'Checkout', elem: 'Steps' } }
+                label={ __('Checkout page') }
+                >
+                    { this.renderSteps() }
+                </ContentWrapper>
+                
+                <ContentWrapper
+                  wrapperMix={ { block: 'Checkout', elem: 'Wrapper' } }
+                  label={ __('Checkout page') }
+                >
+                    { this.renderSummary(true) }
+                    <div block="Checkout" elem="Step">
+                        { this.renderTitle() }
+                        { this.renderGuestForm() }
+                        { this.renderStep() }
+                        { this.renderLoader() }
+                    </div>
+                    <div>
+                        { this.renderSummary() }
+                        { this.renderPromo() }
+                        { this.renderCoupon() }
+                    </div>
+                </ContentWrapper>
+            </main>
+    )
   }
 
 }
+
+// export * from 'SourceRoute/Checkout/Checkout.component';
+// export { default } from 'SourceRoute/Checkout/Checkout.component';
 
 export default CheckoutComponent
